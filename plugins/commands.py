f7b99eda266fc7105d63982928c60f29209e0dfc
Anand <114323379+DrDevAnand@users.noreply.github.com>
2023-01-01 19:46:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,6 +244,8 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('💥SᴜʙSᴄʀɪʙᴇ💥', url='https://youtube.com/@anandmoviesofficial')] ] ),
+
         protect_content=True if pre == 'filep' else False,
         )
                     
