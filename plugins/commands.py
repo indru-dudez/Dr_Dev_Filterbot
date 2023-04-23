8508141ff714ab8e57309015a89a382fcef9c303
Anand <114323379+DrDevAnand@users.noreply.github.com>
2023-04-23 15:25:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -81,7 +81,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
+            text="**Please Join My Updates Channel to use this Bot! ಮೇನ್(Updates) ಚಾನೆಲ್ ಅನ್ನು ಸೇರಿ ಆಮೇಲೆ Try Again👇**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
             )
