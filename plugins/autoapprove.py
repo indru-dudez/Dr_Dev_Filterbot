5b1951ebc2edea4533a45c05dc28eaf59f972855
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2023-08-02 13:05:57 +0530

    Create autoapprove.py

---
+++ plugins/autoapprove.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters 
+
+CHAT_ID = [] #"Working Group ID "
+TEXT = "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved"
+APPROVED = "on"
+
+
+@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client, message: ChatJoinRequest):
+    chat=message.chat # Chat
+    user=message.from_user # User
+    print(f"{user.first_name} Joined 🤝") # Logs
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    if APPROVED == "on":
+        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
